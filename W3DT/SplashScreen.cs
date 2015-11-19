d833e81efe94c1f63dec24f1030aa1c98f808be1
Kruithne <kruithne@gmail.com>
2015-11-19 06:30:59 +0000

    Minor extensions to update system

--- W3DT/SplashScreen.cs
+++ W3DT/SplashScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 using W3DT.Events;
 using W3DT.Runners;
 using W3DT.JSONContainers;
@@ -16,11 +17,25 @@ namespace W3DT
     public partial class SplashScreen : Form
     {
         private bool isDoneLoading = false;
+        public static string UPDATE_PACKAGE_NAME = "update.zip";
 
         public SplashScreen()
         {
             InitializeComponent();
             EventManager.H_UpdateCheckComplete += OnUpdateCheckComplete;
+
+            // Check for and remove leftover update package.
+            try
+            {
+                if (File.Exists(UPDATE_PACKAGE_NAME))
+                    File.Delete(UPDATE_PACKAGE_NAME);
+            }
+            catch (Exception ex)
+            {
+                // File locked, or something. Brazenly continue onwards.
+                Debug.WriteLine("Unable to delete leftover package file (" + ex.GetType().Name + ")");
+                Debug.WriteLine("Exception info: " + ex.Message);
+            }
 
             new RunnerUpdateCheck().Begin();
         }
@@ -36,7 +51,12 @@ namespace W3DT
             else
             {
                 Debug.WriteLine("Remote latest version: " + data.tag_name);
-                // ToDo: Compare versions to our own and initiate update.
+                
+                // ToDo:
+                // - Compare data.tag_name with current version (define this somewhere).
+                // - If remote version is newer, download package async.
+                // - Once download complete event procs, abort app load and launch updater.
+                // - Updater will wait (and try to engage) the closing of this app before unpacking.
             }
 
             EventManager.H_UpdateCheckComplete -= OnUpdateCheckComplete; // Unregister event.
