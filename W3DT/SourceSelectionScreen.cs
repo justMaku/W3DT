6b93d07c09be172f43954f568d8e705daf82fc7d
Kruithne <kruithne@gmail.com>
2015-11-25 07:40:14 +0000

    Fix: Logic ordering for source selector was a little out of shape

--- W3DT/SourceSelectionScreen.cs
+++ W3DT/SourceSelectionScreen.cs
@@ -30,38 +30,9 @@ namespace W3DT
 
         private void UI_SourceOptions_Done_Click(object sender, EventArgs e)
         {
-            string selectedDirectory = UI_DirectoryField.Text;
+            bool useRemote = UI_SourceOption_Remote.Checked;
 
-            // Ensure the user didn't leave the field "blank".
-            if (selectedDirectory == string.Empty || selectedDirectory == Constants.DIRECTORY_PLACEHOLDER || selectedDirectory == null)
-            {
-                ShowBalloon("There must always be, a Lich K-- .. I mean, this can't be blank!", UI_DirectorySelectButton);
-                return;
-            }
-
-            // Ensure the given directory actually exists.
-            if (!Directory.Exists(selectedDirectory))
-            {
-                ShowBalloon("The directory you selected could not be found, even by Brann Bronzebeard!", UI_DirectorySelectButton);
-                return;
-            }
-
-            // Rough check to see if we picked a valid installation.
-            string buildFile = Path.Combine(selectedDirectory, Constants.WOW_BUILD_FILE);
-            if (!File.Exists(buildFile))
-            {
-                ShowBalloon("That does not appear to be a valid World of Warcraft installation..", UI_DirectorySelectButton);
-                return;
-            }
-
-            selectionDone = true;
-
-            Program.Settings.UseRemote = UI_SourceOption_Remote.Checked;
-            Program.Settings.ShowSourceSelector = false;
-            Program.Settings.WoWDirectory = selectedDirectory;
-            Program.Settings.Persist();
-
-            if (Program.Settings.UseRemote)
+            if (useRemote)
             {
                 EventManager.CDNScanDone += OnCDNSearchDone;
                 cdnWindow = new CDNSearchWindow(this);
@@ -69,6 +40,36 @@ namespace W3DT
             }
             else
             {
+                string selectedDirectory = UI_DirectoryField.Text;
+
+                // Ensure the user didn't leave the field "blank".
+                if (selectedDirectory == string.Empty || selectedDirectory == Constants.DIRECTORY_PLACEHOLDER || selectedDirectory == null)
+                {
+                    ShowBalloon("There must always be, a Lich K-- .. I mean, this can't be blank!", UI_DirectorySelectButton);
+                    return;
+                }
+
+                // Ensure the given directory actually exists.
+                if (!Directory.Exists(selectedDirectory))
+                {
+                    ShowBalloon("The directory you selected could not be found, even by Brann Bronzebeard!", UI_DirectorySelectButton);
+                    return;
+                }
+
+                // Rough check to see if we picked a valid installation.
+                string buildFile = Path.Combine(selectedDirectory, Constants.WOW_BUILD_FILE);
+                if (!File.Exists(buildFile))
+                {
+                    ShowBalloon("That does not appear to be a valid World of Warcraft installation..", UI_DirectorySelectButton);
+                    return;
+                }
+
+                Program.Settings.UseRemote = false;
+                Program.Settings.ShowSourceSelector = false;
+                Program.Settings.WoWDirectory = selectedDirectory;
+                Program.Settings.Persist();
+
+                selectionDone = true;
                 DeferToParent();
             }
         }
@@ -87,6 +88,8 @@ namespace W3DT
             {
                 Program.Settings.RemoteHost = bestHost;
                 Program.Settings.WoWDirectory = null;
+                Program.Settings.ShowSourceSelector = false;
+                Program.Settings.UseRemote = true;
                 Program.Settings.Persist();
 
                 DeferToParent();
