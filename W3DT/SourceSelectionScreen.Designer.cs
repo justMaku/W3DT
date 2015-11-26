9ce83d160012288d04d6dda3229e9d193893c2b0
Kruithne <kruithne@gmail.com>
2015-11-26 01:03:47 +0000

    Minor: Update source selection text to include beta note

--- W3DT/SourceSelectionScreen.Designer.cs
+++ W3DT/SourceSelectionScreen.Designer.cs
@@ -189,9 +189,9 @@
             this.label14.ForeColor = System.Drawing.Color.Green;
             this.label14.Location = new System.Drawing.Point(23, 205);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(91, 13);
+            this.label14.Size = new System.Drawing.Size(189, 13);
             this.label14.TabIndex = 3;
-            this.label14.Text = "Always up-to-date";
+            this.label14.Text = "Latest version (Even unreleased beta!)";
             // 
             // label13
             // 
