d76072dc54ee7ac132ecb928aaa4e88d36916b2b
Kruithne <kruithne@gmail.com>
2016-01-27 21:10:47 +0000

    Base layout of WMO chunk MOHD added

---
+++ W3DT/3D/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using W3DT.Formats;
+
+namespace W3DT._3D
+{
+    class Position
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Z { get; private set; }
+
+        public Position(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Position Read(FormatBase input)
+        {
+            return new Position(input.readFloat(), input.readFloat(), input.readFloat());
+        }
+    }
+}
---
+++ W3DT/Formats/WMO/Chunk_MOHD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using W3DT._3D;
+
+namespace W3DT.Formats.WMO
+{
+    public class Chunk_MOHD : Chunk_Base
+    {
+        public const UInt32 Magic = 0x4D4F4844;
+
+        public UInt32 nTextures { get; private set; }
+        public UInt32 nGroups { get; private set; }
+        public UInt32 nPortals { get; private set; }
+        public UInt32 nLights { get; private set; }
+        public UInt32 nDoodadNames { get; private set; }
+        public UInt32 nDoodadRefs { get; private set; }
+        public UInt32 nDoodadSets { get; private set; }
+        public UInt32 colourR { get; private set; }
+        public UInt32 colourG { get; private set; }
+        public UInt32 colourB { get; private set; }
+        public UInt32 colourX { get; private set; }
+        public UInt32 wmoID { get; private set; }
+        public Position boundingBoxLow { get; private set; }
+        public Position boundingBoxHigh { get; private set; }
+        public UInt32 flags { get; private set; }
+
+        public Chunk_MOHD(WMOFile file) : base(file)
+        {
+            ChunkID = Magic;
+
+            // ToDo: Populate variables.
+
+            // ToDo: Logging
+        }
+    }
+}
