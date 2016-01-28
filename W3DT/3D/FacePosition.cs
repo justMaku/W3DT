8540ae0aedbb14c038d31a77cbf1f20a67b815c2
Kruithne <kruithne@gmail.com>
2016-01-28 22:15:19 +0000

    Added FacePosition class

---
+++ W3DT/3D/FacePosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using W3DT.Formats;
+
+namespace W3DT._3D
+{
+    public class FacePosition
+    {
+        public UInt16 point1 { get; private set; }
+        public UInt16 point2 { get; private set; }
+        public UInt16 point3 { get; private set; }
+
+        public FacePosition(UInt16 p1, UInt16 p2, UInt16 p3)
+        {
+            point1 = p1;
+            point2 = p2;
+            point3 = p3;
+        }
+
+        public static FacePosition Read(FormatBase input)
+        {
+            return new FacePosition(input.readUInt16(), input.readUInt16(), input.readUInt16());
+        }
+    }
+}
