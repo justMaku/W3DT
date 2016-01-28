5a54f6a697fe090eb0cc94a0883935724fc15efc
Kruithne <kruithne@gmail.com>
2016-01-28 22:19:30 +0000

    Added structure for MOVI chunks (WMO)

---
+++ W3DT/Formats/WMO/Chunk_MOVI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace W3DT.Formats.WMO
+{
+    public class Chunk_MOVI : Chunk_Base
+    {
+        // MOVI WMO Chunk
+        // Map verts to faces.
+
+        public const UInt32 Magic = 0x4D4F5649;
+        public FacePosition[] positions { get; private set; }
+
+        public Chunk_MOVI(WMOFile file) : base(file, "MOVI")
+        {
+            ChunkID = Magic;
+
+            int faceCount = (int)ChunkSize / 6;
+            positions = new FacePosition[faceCount];
+
+            for (int i = 0; i < faceCount; i++)
+                positions[i] = FacePosition.Read(file);
+
+            LogWrite("Loaded " + faceCount + " vert -> face mappings.");
+        }
+    }
+}
