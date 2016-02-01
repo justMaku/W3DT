46091700d6fdedca137b0089e30d1c3c3a1c09c2
Kruithne <kruithne@gmail.com>
2016-02-01 19:16:09 +0000

    Improve exploration filtering

--- W3DT/Runners/RunnerFileExplore.cs
+++ W3DT/Runners/RunnerFileExplore.cs
@@ -11,8 +11,17 @@ namespace W3DT.Runners
 {
     class RunnerFileExplore : RunnerBase
     {
+        private enum FilterType
+        {
+            CONTAINS,
+            STARTS_WITH,
+            ENDS_WITH,
+            EQUAL
+        };
+
         private string id;
         private string filter;
+        private FilterType filterType = FilterType.CONTAINS;
         private string[] extensions;
 
         public RunnerFileExplore(string id, string[] extensions, string filter = null)
@@ -20,6 +29,23 @@ namespace W3DT.Runners
             this.id = id;
             this.filter = filter;
             this.extensions = extensions;
+
+            // Pre-compile the filter parameters.
+            if (filter != null)
+            {
+                char first = filter.Substring(0, 1)[0];
+                if (first == '=')
+                    filterType = FilterType.EQUAL;
+                else if (first == '^')
+                    filterType = FilterType.STARTS_WITH;
+                else if (first == '$')
+                    filterType = FilterType.ENDS_WITH;
+
+                if (filterType != FilterType.CONTAINS)
+                    this.filter = filter.Substring(1, filter.Length - 1);
+
+                Log.Write("RunnerFileExplore: Using filter {0} [{1}] for search.", filter, filterType.ToString());
+            }
         }
 
         public override void Work()
@@ -42,10 +68,37 @@ namespace W3DT.Runners
                 else if (entry is CASCFile)
                 {
                     CASCFile file = (CASCFile)entry;
-                    if ((filter == null || file.FullName.ToLower().Contains(filter)) && IsValidExtension(file))
-                        EventManager.Trigger_FileExploreHit(new FileExploreHitArgs(id, file));
+
+                    // Check we have a valid extension.
+                    if (!IsValidExtension(file))
+                        continue;
+
+                    // Ensure the file matches the filter.
+                    if (!MatchesFilter(file.FullName))
+                        continue;
+
+                    EventManager.Trigger_FileExploreHit(new FileExploreHitArgs(id, file));
                 }
             }
+        }
+
+        private bool MatchesFilter(string file)
+        {
+            if (filter == null)
+                return true;
+
+            file = file.ToLower();
+            if (filterType == FilterType.CONTAINS)
+                return file.Contains(filter);
+
+            if (filterType == FilterType.STARTS_WITH)
+                return file.StartsWith(filter);
+
+            if (filterType == FilterType.ENDS_WITH)
+                return file.EndsWith(filter);
+
+            // Default to FilterType.EQUALS
+            return file == filter;
         }
 
         private bool IsValidExtension(CASCFile file)
