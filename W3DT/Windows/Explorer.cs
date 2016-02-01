5fd7e2a9e948c68d172fa20f85072da74f6ae34f
Kruithne <kruithne@gmail.com>
2016-02-01 19:46:47 +0000

    Filters should always be lower-case

--- W3DT/Windows/Explorer.cs
+++ W3DT/Windows/Explorer.cs
@@ -66,7 +66,7 @@ namespace W3DT
                 }
                 else
                 {
-                    hardFilter = searchField.ToString();
+                    hardFilter = searchField.ToString().ToLower();
                 }
             }
         }
