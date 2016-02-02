78026d58753469e66c710477f785bf48edfbc3b1
Kruithne <kruithne@gmail.com>
2016-02-02 15:57:54 +0000

    Finish core implementation of map viewer

--- W3DT/Runners/RunnerMapBuilder.cs
+++ W3DT/Runners/RunnerMapBuilder.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
 using W3DT.Events;
+using SereniaBLPLib;
 
 namespace W3DT.Runners
 {
     public class RunnerMapBuilder : RunnerBase
     {
+        private Regex pattern = new Regex(@"^map(\d+)_(\d+)$");
         private string[] files;
 
         public RunnerMapBuilder(string[] files)
@@ -17,17 +22,61 @@ namespace W3DT.Runners
 
         public override void Work()
         {
-            /*
-             * ToDo:
-             * - Go through each of the files given, construct map.
-             * - Sort maps.
-             * - Extract map tiles.
-             * - Load every tile as BLP.
-             * - Stich tiles together on canvas.
-             */
+            SortedDictionary<int, SortedDictionary<int, string>> map = new SortedDictionary<int, SortedDictionary<int, string>>();
 
-            // ToDo: Actually pass a bitmap back here.
-            EventManager.Trigger_MapBuildDone(null);
+            int xSize = 0;
+            int ySize = 0;
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                Match match = pattern.Match(fileName);
+
+                if (match.Success)
+                {
+                    int xx = int.Parse(match.Groups[1].Value);
+                    int yy = int.Parse(match.Groups[2].Value);
+
+                    if (!map.ContainsKey(xx))
+                    {
+                        map.Add(xx, new SortedDictionary<int, string>());
+                        xSize++;
+                    }
+
+                    SortedDictionary<int, string> node = map[xx];
+                    node.Add(yy, file);
+
+                    if (node.Count > ySize)
+                        ySize = node.Count;
+                }
+            }
+
+            Bitmap canvas = new Bitmap(xSize * 256, ySize * 256);
+            using (Graphics gfx = Graphics.FromImage(canvas))
+            {
+                int iX = 0;
+                foreach (KeyValuePair<int, SortedDictionary<int, string>> node in map)
+                {
+                    int iY = 0;
+                    foreach (KeyValuePair<int, string> subNode in node.Value)
+                    {
+                        if (File.Exists(subNode.Value))
+                        {
+                            using (BlpFile blp = new BlpFile(File.OpenRead(subNode.Value)))
+                            {
+                                gfx.DrawImage(blp.GetBitmap(0), new Rectangle(iX * 256, iY * 256, 256, 256), new Rectangle(0, 0, 256, 256), GraphicsUnit.Pixel);
+
+                                // ! Debugging !
+                                Log.Write("Stitching {0} at {1} x {2}", subNode.Value, iX * 256, iY * 256);
+                            }
+                        }
+
+                        iY++;
+                    }
+                    iX++;
+                }
+            }
+
+            EventManager.Trigger_MapBuildDone(new MapBuildDoneArgs(canvas));
         }
     }
 }
--- W3DT/Windows/MapViewerWindow.cs
+++ W3DT/Windows/MapViewerWindow.cs
@@ -18,14 +18,25 @@ namespace W3DT
     public partial class MapViewerWindow : Form
     {
         private Explorer explorer;
-        private Dictionary<string, List<string>> maps;
+        private Dictionary<string, List<CASCFile>> maps;
         private RunnerMapBuilder runner;
+
+        // File extraction
+        private List<ExtractState> requiredFiles;
+        private List<RunnerExtractItem> runners;
+        private List<string> paths;
+
+        private Bitmap image;
 
         public MapViewerWindow()
         {
             InitializeComponent();
 
-            maps = new Dictionary<string, List<string>>();
+            maps = new Dictionary<string, List<CASCFile>>();
+
+            requiredFiles = new List<ExtractState>();
+            runners = new List<RunnerExtractItem>();
+            paths = new List<string>();
 
             explorer = new Explorer(this, "^World\\Minimaps\\", null, UI_FilterTimer, null, null, new string[] { "blp" }, "MVT_N_{0}", true);
             explorer.ExploreHitCallback = OnExploreHit;
@@ -33,12 +44,38 @@ namespace W3DT
 
             EventManager.MapBuildDone += OnMapBuildDone;
             EventManager.CASCLoadStart += OnCASCLoadStart;
+            EventManager.FileExtractComplete += OnFileExtractComplete;
             explorer.Initialize();
         }
 
         private void ClearMap()
         {
-            UI_Map.CreateGraphics().Clear(UI_Map.BackColor);
+            using (Graphics gfx = UI_Map.CreateGraphics())
+                gfx.Clear(UI_Map.BackColor);
+        }
+
+        private void RenderImage()
+        {
+            using (Graphics gfx = UI_Map.CreateGraphics())
+            {
+                gfx.Clear(UI_Map.BackColor);
+                gfx.DrawImage(image, 0, 0);
+            }
+        }
+
+        private void TerminateRunners()
+        {
+            // Kill existing map runner if it's already going.
+            if (runner != null)
+                runner.Kill();
+
+            runner = null;
+
+            // Kill extraction runners.
+            foreach (RunnerExtractItem extractRunner in runners)
+                extractRunner.Kill();
+
+            runners.Clear();
         }
 
         private void OnExploreHit(CASCFile file)
@@ -51,10 +88,10 @@ namespace W3DT
                 if (!maps.ContainsKey(mapName))
                 {
                     UI_FileList.Nodes.Add(mapName);
-                    maps.Add(mapName, new List<string>());
+                    maps.Add(mapName, new List<CASCFile>());
                 }
 
-                maps[mapName].Add(file.Name);
+                maps[mapName].Add(file);
                 UpdateSearchState(Constants.SEARCH_STATE_SEARCHING);
             }
         }
@@ -77,7 +114,10 @@ namespace W3DT
         private void OnMapBuildDone(object sender, EventArgs e)
         {
             MapBuildDoneArgs args = (MapBuildDoneArgs)e;
-            // ToDo: Use the bitmap given here to display the map.
+            image = args.Data;
+            RenderImage();
+
+            UI_PreviewStatus.Hide();
         }
 
         private void UI_FileList_AfterSelect(object sender, TreeViewEventArgs e)
@@ -86,22 +126,75 @@ namespace W3DT
 
             if (selected != null)
             {
-                // Kill existing runner if it's already going.
-                if (runner != null)
-                    runner.Kill();
-
-                // Clear map background.
-                ClearMap();
+                // Clean up previous excursions.
+                TerminateRunners(); // Terminate runners that be running.
+                ClearMap(); // Clear map background.
+                requiredFiles.Clear(); // Clear required file list.
+                paths.Clear(); // Clear paths cache.
 
                 string mapName = selected.Text;
                 UI_PreviewStatus.Text = string.Format(Constants.MAP_VIEWER_LOADING_MAP, mapName);
+                UI_PreviewStatus.Show();
 
-                runner = new RunnerMapBuilder(maps[mapName].ToArray());
+                foreach (CASCFile file in maps[mapName])
+                {
+                    string tempPath = Path.Combine(Constants.TEMP_DIRECTORY, file.FullName);
+                    ExtractState state = new ExtractState(file);
+
+                    if (!File.Exists(tempPath))
+                    {
+                        RunnerExtractItem extractRunner = new RunnerExtractItem(file);
+                        state.TrackerID = extractRunner.runnerID;
+                        state.State = false;
+
+                        extractRunner.Begin();
+                        runners.Add(extractRunner);
+                    }
+                    else
+                    {
+                        state.State = true;
+                    }
+
+                    requiredFiles.Add(state);
+                    paths.Add(tempPath);
+                }
+            }
+        }
+
+        private void BeginMapBuild()
+        {
+            // Minor clean-up here, probably not needed.
+            requiredFiles.Clear();
+
+            runner = new RunnerMapBuilder(paths.ToArray());
+            runner.Begin();
+        }
+
+        private void OnFileExtractComplete(object sender, EventArgs e)
+        {
+            FileExtractCompleteArgs args = (FileExtractCompleteArgs)e;
+
+            ExtractState state = requiredFiles.Where(s => s.TrackerID == args.RunnerID).FirstOrDefault();
+            if (state != null)
+            {
+                // Note: We don't actually check for success here.
+                // It the tile cannot be extracted, we'll just render nothing in it's place.
+
+                state.State = true;
+
+                if (!requiredFiles.Any(s => !s.State))
+                    BeginMapBuild();
             }
         }
 
         private void MapViewerWindow_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // Unregister events.
+            EventManager.MapBuildDone -= OnMapBuildDone;
+            EventManager.CASCLoadStart -= OnCASCLoadStart;
+            EventManager.FileExtractComplete -= OnFileExtractComplete;
+
+            TerminateRunners();
             explorer.Dispose();
         }
 
