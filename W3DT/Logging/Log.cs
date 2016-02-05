ccaad9bd5ef261f6c6d7e79dd4a9fd3e1bf7d8fe
Kruithne <kruithne@gmail.com>
2016-02-05 15:41:19 +0000

    Optimization: Async logging

---
+++ W3DT/Logging/Log.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using W3DT.Logging;
+
+namespace W3DT
+{
+    class Log
+    {
+        public static Queue<string> Pipe;
+        private static Lumbermill runner;
+
+        public static void Initialize(string file)
+        {
+            if (File.Exists(file))
+                File.Delete(file);
+
+            Pipe = new Queue<string>();
+
+            runner = new Lumbermill(new StreamWriter(file, true));
+            runner.Begin();
+
+            string year = DateTime.Now.ToString("yyyy");
+            string monthDay = DateTime.Now.ToString("MMdd");
+
+            Write(string.Format("Captain's log, USS Enterprise (NCC-{0}-D), {1}", year, monthDay));
+        }
+
+        public static void Write(string message, params object[] args)
+        {
+            Write(string.Format(message, args));
+        }
+
+        public static void Write(string message)
+        {
+            Pipe.Enqueue(message);
+        }
+
+        public static void Dispose()
+        {
+            if (runner != null)
+            {
+                runner.Kill();
+                runner = null;
+            }
+        }
+    }
+}
---
+++ W3DT/Logging/Lumbermill.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using W3DT.Runners;
+
+namespace W3DT.Logging
+{
+    public class Lumbermill : RunnerBase
+    {
+        private TextWriter writer;
+
+        public Lumbermill(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public override void Work()
+        {
+            while (true)
+            {
+                if (Log.Pipe.Count > 0)
+                {
+                    string message = Log.Pipe.Dequeue();
+
+                    // Write to the log file.
+                    writer.WriteLine(string.Format("[{0}] {1}", getTimestamp(), message));
+                    writer.Flush();
+
+                    // Write to the debugger.
+                    Debug.WriteLine(message);
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private string getTimestamp()
+        {
+            return DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.ff");
+        }
+
+        private bool isValid()
+        {
+            return writer != null;
+        }
+
+        public new void Kill()
+        {
+            if (isValid())
+                writer.Close();
+
+            if (thread != null && thread.IsAlive)
+                thread.Abort();
+        }
+    }
+}
