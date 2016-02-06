5b2bf77fea436755e9ced167b2c70ddfca15e141
Kruithne <kruithne@gmail.com>
2016-02-06 23:09:59 +0000

    Add: Base for ADTFile parser

---
+++ W3DT/Formats/ADTFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace W3DT.Formats
+{
+    public class ADTException : Exception
+    {
+        public ADTException(string message) : base(message) { }
+    }
+
+    public class ADTFile : FormatBase
+    {
+        public string BaseName { get; private set; }
+
+        public ADTFile(string file) : base(file)
+        {
+            BaseName = Path.GetFileName(file);
+        }
+
+        public void parse()
+        {
+            // ToDo: Actually parse the file.
+        }
+    }
+}
