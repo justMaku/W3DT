7c5a34e3e6abebe54411a98afe64cf107f5ee7c2
Kruithne <kruithne@gmail.com>
2016-02-10 00:19:29 +0000

    Minor: Chunk fields should all be pascal-case

--- W3DT/Formats/ADT/Chunk_MMDX.cs
+++ W3DT/Formats/ADT/Chunk_MMDX.cs
@@ -8,12 +8,12 @@ namespace W3DT.Formats.ADT
     public class Chunk_MMDX : Chunk_Base
     {
         public const UInt32 Magic = 0x4D4D4458;
-        public StringBlock Models { get; private set; }
+        public StringBlock models { get; private set; }
 
         public Chunk_MMDX(ADTFile file) : base(file, "MMDX")
         {
             ChunkID = Magic;
-            Models = new StringBlock(file, (int)ChunkSize);
+            models = new StringBlock(file, (int)ChunkSize);
         }
     }
 }
--- W3DT/Formats/ADT/Chunk_MTEX.cs
+++ W3DT/Formats/ADT/Chunk_MTEX.cs
@@ -8,12 +8,12 @@ namespace W3DT.Formats.ADT
     public class Chunk_MTEX : Chunk_Base
     {
         public const UInt32 Magic = 0x4D544558;
-        public StringBlock Textures { get; private set; }
+        public StringBlock textures { get; private set; }
 
         public Chunk_MTEX(ADTFile file) : base(file, "MTEX")
         {
             ChunkID = Magic;
-            Textures = new StringBlock(file, (int)ChunkSize);
+            textures = new StringBlock(file, (int)ChunkSize);
         }
     }
 }
