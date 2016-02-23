7c7ea748db17012a142cc6089439ca6cc45d1141
Kruithne <kruithne@gmail.com>
2016-02-23 00:01:26 +0000

    UI: Show some form of progress with the loading window

--- W3DT/Runners/RunnerMapExport.cs
+++ W3DT/Runners/RunnerMapExport.cs
@@ -172,6 +172,8 @@ namespace W3DT.Runners
                                         string texFileName = string.Format("baked_{0}_{1}_{2}.png", x, y, i);
                                         string texFilePath = Path.Combine(dataDir, texFileName);
 
+                                        EventManager.Trigger_LoadingPrompt(string.Format("Rendering tile {0} at {1},{2}...", i, x, y));
+
                                         if (!File.Exists(texFilePath))
                                         {
                                             Bitmap bmpBase = new Bitmap(textureData[layers.layers[0].textureID]);
