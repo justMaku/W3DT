1e96c105998ef9338ae8177586b6e11c069c16fd
Kruithne <kruithne@gmail.com>
2016-02-26 23:31:41 +0000

    Fix: Remove top anchor from colour select buttons

--- W3DT/Windows/ModelViewer.Designer.cs
+++ W3DT/Windows/ModelViewer.Designer.cs
@@ -117,8 +117,7 @@
             // 
             // UI_ColourChangeButton
             // 
-            this.UI_ColourChangeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.UI_ColourChangeButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.UI_ColourChangeButton.Image = global::W3DT.Properties.Resources.paint;
             this.UI_ColourChangeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
