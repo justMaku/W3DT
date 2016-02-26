701f847347dde32b5a52cf954da76449bb19e840
Kruithne <kruithne@gmail.com>
2016-02-26 02:51:47 +0000

    More engine work on the model viewer

--- W3DT/Formats/M2File.cs
+++ W3DT/Formats/M2File.cs
@@ -13,15 +13,64 @@ namespace W3DT.Formats
 
     public class M2File : FormatBase
     {
+        private static uint MD21_MAGIC = 0x3132444D;
+        private static uint MD20_MAGIC = 0x3032444D;
+
+        public uint Version { get; private set; }
+        public string Name { get; private set; }
+
         public M2File(string file) : base(file) { }
 
         public override void parse()
         {
-            string magic = readString(4);
-            if (magic.Equals("MD20"))
+            int ofs = 0;
+            uint magic = readUInt32();
+
+            if (!magic.Equals(MD20_MAGIC)) // Assume Legion+ chunked format, relate offset to chunk.
             {
-                // Pre-Legion
+                seekPosition(0); // Go back to the start.
+                while (!isEndOfStream() && !isOutOfBounds(seek + 8))
+                {
+                    uint chunkID = readUInt32();
+                    uint chunkSize = readUInt32();
+
+                    if (chunkID == MD21_MAGIC)
+                    {
+                        ofs = seek;
+                        break;
+                    }
+                    else
+                    {
+                        skip((int)chunkSize);
+                    }
+                }
+
+                if (ofs == 0)
+                    throw new M2Exception("Unable to find MD20 chunk inside MD21 file! Format evolved?");
             }
+
+            // Start reading as MD20
+            seekPosition(ofs); // Seek the beginning of the MD20 data.
+            magic = readUInt32();
+
+            if (magic != MD20_MAGIC)
+                throw new M2Exception(string.Format("Data is not MD20 at {0}", ofs));
+
+            /* 
+             * 274+ Legion
+             * ? - ? Warlords of Draenor
+             * 273 Mists of Pandaria
+             * 265-272 Cataclysm
+             * 264 Wrath of the Lich King
+             * 260-263 The Burning Crusade
+             * ?-256 World of Warcraft
+             */
+            Version = readUInt32();
+            int iName = (int)readUInt32(); // Contains trailing 0-byte.
+            int ofsName = (int)readUInt32();
+
+            seekPosition(ofs + ofsName);
+            Name = readString(iName - 1);
         }
     }
 }
--- W3DT/Windows/ModelViewer.cs
+++ W3DT/Windows/ModelViewer.cs
@@ -8,16 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.IO;
 using W3DT.CASC;
 using W3DT.Runners;
 using W3DT.Events;
+using W3DT.Formats;
 
 namespace W3DT
 {
     public partial class ModelViewer : Form
     {
         private Explorer explorer;
+
         private RunnerExtractItem runner;
+        private LoadingWindow loadingWindow;
+        private Action cancelCallback;
+
+        private string selectedFileName;
 
         public ModelViewer()
         {
@@ -27,6 +34,7 @@ namespace W3DT
             explorer.rootFolders = new string[] { "character", "creature" };
             explorer.Initialize();
 
+            cancelCallback = CancelLoad;
             EventManager.FileExtractComplete += EventManager_FileExtractComplete;
         }
 
@@ -39,13 +47,54 @@ namespace W3DT
             }
         }
 
+        private void CloseLoadingWindow()
+        {
+            if (loadingWindow != null)
+            {
+                if (!loadingWindow.IsDisposed && loadingWindow.Visible)
+                    loadingWindow.Close();
+
+                loadingWindow = null;
+            }
+        }
+
+        private void CancelLoad()
+        {
+            TerminateRunners();
+            CloseLoadingWindow();
+        }
+
         private void EventManager_FileExtractComplete(object sender, EventArgs e)
         {
             FileExtractCompleteArgs args = (FileExtractCompleteArgs)e;
 
             if (runner != null && args.RunnerID == runner.runnerID)
             {
-                // ToDo: Load model.
+                string tempPath = Path.Combine(Constants.TEMP_DIRECTORY, args.File.FullName);
+
+                try
+                {
+                    if (File.Exists(tempPath))
+                    {
+                        M2File model = new M2File(tempPath);
+                        model.parse();
+
+                        Log.Write("ModelViewer: Loaded {0} M2 data.", model.Name);
+
+                        // ToDo: Download textures.
+                        // ToDo: Compile M2 data into a mesh and render it.
+                    }
+                    else
+                    {
+                        throw new M2Exception("Extracted file does not exist: " + tempPath);
+                    }
+                }
+                catch (M2Exception ex)
+                {
+                    Alert.Show(string.Format("Sorry, an error prevented {0} from being opened!", selectedFileName));
+                    Log.Write("Unable to extract M2 file: " + ex.Message);
+                    Log.Write(ex.StackTrace);
+                }
             }
         }
 
@@ -57,6 +106,10 @@ namespace W3DT
                 CASCFile file = (CASCFile)node.Tag;
                 TerminateRunners();
 
+                loadingWindow = new LoadingWindow(string.Format("Loading {0} model...", selectedFileName), "Extracting from data source...", true, cancelCallback);
+                loadingWindow.ShowDialog();
+
+                selectedFileName = file.Name;
                 runner = new RunnerExtractItem(file);
                 runner.Begin();
             }
