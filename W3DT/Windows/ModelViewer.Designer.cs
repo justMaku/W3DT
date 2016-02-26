cbee9fdfd56490f03607fbc8f72c0163be9ad4d6
Kruithne <kruithne@gmail.com>
2016-02-26 05:09:44 +0000

    UI: Introduce common engine stuff to model viewer

--- W3DT/Windows/ModelViewer.Designer.cs
+++ W3DT/Windows/ModelViewer.Designer.cs
@@ -30,27 +30,38 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModelViewer));
-            this.openGLControl = new SharpGL.OpenGLControl();
+            this.UI_3DView = new SharpGL.OpenGLControl();
             this.UI_FilterField = new System.Windows.Forms.TextBox();
             this.UI_FileCount_Label = new System.Windows.Forms.Label();
             this.UI_FileList = new System.Windows.Forms.TreeView();
             this.UI_FilterOverlay = new System.Windows.Forms.Label();
             this.UI_FilterTime = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.UI_ColourDialog = new System.Windows.Forms.ColorDialog();
+            this.UI_ColourChangeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_3DView)).BeginInit();
             this.SuspendLayout();
             // 
-            // openGLControl
+            // UI_3DView
             // 
-            this.openGLControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.openGLControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.openGLControl.DrawFPS = false;
-            this.openGLControl.Location = new System.Drawing.Point(333, 12);
-            this.openGLControl.Name = "openGLControl";
-            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
-            this.openGLControl.RenderContextType = SharpGL.RenderContextType.FBO;
-            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-            this.openGLControl.Size = new System.Drawing.Size(788, 475);
-            this.openGLControl.TabIndex = 1;
+            this.UI_3DView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_3DView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UI_3DView.DrawFPS = false;
+            this.UI_3DView.Location = new System.Drawing.Point(261, 12);
+            this.UI_3DView.Name = "UI_3DView";
+            this.UI_3DView.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.UI_3DView.RenderContextType = SharpGL.RenderContextType.FBO;
+            this.UI_3DView.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.UI_3DView.Size = new System.Drawing.Size(686, 475);
+            this.UI_3DView.TabIndex = 1;
+            this.UI_3DView.OpenGLInitialized += new System.EventHandler(this.UI_3DView_OpenGLInitialized);
+            this.UI_3DView.OpenGLDraw += new SharpGL.RenderEventHandler(this.UI_3DView_OpenGLDraw);
+            this.UI_3DView.Resized += new System.EventHandler(this.UI_3DView_Resized);
+            this.UI_3DView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UI_3DView_MouseDown);
+            this.UI_3DView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UI_3DView_MouseMove);
+            this.UI_3DView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_3DView_MouseUp);
+            this.UI_3DView.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.UI_3DView_MouseWheel);
             // 
             // UI_FilterField
             // 
@@ -78,7 +89,7 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.UI_FileList.Location = new System.Drawing.Point(12, 12);
             this.UI_FileList.Name = "UI_FileList";
-            this.UI_FileList.Size = new System.Drawing.Size(315, 579);
+            this.UI_FileList.Size = new System.Drawing.Size(238, 579);
             this.UI_FileList.TabIndex = 6;
             this.UI_FileList.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.UI_FileList_AfterSelect);
             // 
@@ -100,21 +111,40 @@
             // 
             this.UI_FilterTime.Interval = 1000;
             // 
+            // UI_ColourDialog
+            // 
+            this.UI_ColourDialog.FullOpen = true;
+            // 
+            // UI_ColourChangeButton
+            // 
+            this.UI_ColourChangeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_ColourChangeButton.Image = global::W3DT.Properties.Resources.paint;
+            this.UI_ColourChangeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.UI_ColourChangeButton.Location = new System.Drawing.Point(889, 460);
+            this.UI_ColourChangeButton.Name = "UI_ColourChangeButton";
+            this.UI_ColourChangeButton.Size = new System.Drawing.Size(81, 23);
+            this.UI_ColourChangeButton.TabIndex = 10;
+            this.UI_ColourChangeButton.UseVisualStyleBackColor = true;
+            this.UI_ColourChangeButton.Click += new System.EventHandler(this.UI_ColourChangeButton_Click);
+            // 
             // ModelViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1133, 643);
+            this.ClientSize = new System.Drawing.Size(1180, 643);
             this.Controls.Add(this.UI_FilterOverlay);
             this.Controls.Add(this.UI_FilterField);
             this.Controls.Add(this.UI_FileCount_Label);
             this.Controls.Add(this.UI_FileList);
-            this.Controls.Add(this.openGLControl);
+            this.Controls.Add(this.UI_3DView);
+            this.Controls.Add(this.UI_ColourChangeButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ModelViewer";
             this.Text = "Model Viewer - Warcraft 3D Toolkit";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ModelViewer_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_3DView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,11 +152,13 @@
 
         #endregion
 
-        private SharpGL.OpenGLControl openGLControl;
+        private SharpGL.OpenGLControl UI_3DView;
         private System.Windows.Forms.TextBox UI_FilterField;
         private System.Windows.Forms.Label UI_FileCount_Label;
         private System.Windows.Forms.TreeView UI_FileList;
         private System.Windows.Forms.Label UI_FilterOverlay;
         private System.Windows.Forms.Timer UI_FilterTime;
+        private System.Windows.Forms.ColorDialog UI_ColourDialog;
+        private System.Windows.Forms.Button UI_ColourChangeButton;
     }
 }
--- W3DT/Windows/ModelViewer.cs
+++ W3DT/Windows/ModelViewer.cs
@@ -13,6 +13,8 @@ using W3DT.CASC;
 using W3DT.Runners;
 using W3DT.Events;
 using W3DT.Formats;
+using W3DT._3D;
+using SharpGL;
 
 namespace W3DT
 {
@@ -26,9 +28,23 @@ namespace W3DT
 
         private string selectedFileName;
 
+        // 3D View
+        private float rotationY = 0.0f;
+        private float prevRotY = 0.0f;
+        private float zoom = 1.0f;
+        private bool autoRotate = true;
+        private List<Mesh> meshes;
+
+        // Mouse position cache for 3D rotation
+        private int mouseStartX = 0;
+        private int mouseStartY = 0;
+        private bool isMouseRotating = false;
+
         public ModelViewer()
         {
             InitializeComponent();
+
+            meshes = new List<Mesh>();
 
             explorer = new Explorer(this, UI_FilterField, UI_FilterOverlay, UI_FilterTime, UI_FileCount_Label, UI_FileList, new string[] { "m2", "mdx" }, "M2_V_{0}", true);
             explorer.rootFolders = new string[] { "character", "creature" };
@@ -128,6 +144,116 @@ namespace W3DT
             EventManager.FileExtractComplete -= EventManager_FileExtractComplete;
             EventManager.CASCLoadStart -= EventManager_CASCLoadStart;
             TerminateRunners();
+        }
+
+        private void updateViewerBackground(Color backColour)
+        {
+            float r = (float)backColour.R / 255f;
+            float g = (float)backColour.G / 255f;
+            float b = (float)backColour.B / 255f;
+
+            UI_3DView.OpenGL.ClearColor(r, g, b, 1);
+        }
+
+        private void updateCamera()
+        {
+            OpenGL gl = UI_3DView.OpenGL;
+
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+
+            gl.Perspective(60.0f * zoom, (double)UI_3DView.Width / (double)UI_3DView.Height, 0.01, 900.0);
+            gl.LookAt(50, 20, 50, 0, 0, 0, 0, 1, 0);
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void UI_3DView_OpenGLInitialized(object sender, EventArgs e)
+        {
+            updateCamera();
+            OpenGL gl = UI_3DView.OpenGL;
+
+            gl.DepthFunc(OpenGL.GL_LESS);
+            gl.Enable(OpenGL.GL_CULL_FACE);
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+
+            updateViewerBackground(Color.FromArgb(Program.Settings.ModelViewerBackgroundColour));
+        }
+
+        private void UI_3DView_Resized(object sender, EventArgs e)
+        {
+            updateCamera();
+        }
+
+        private void UI_3DView_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            OpenGL gl = UI_3DView.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            // Auto-correct rotation values to keep them sane.
+            if (rotationY > 360f) rotationY -= 360f; else if (rotationY < -360f) rotationY += 360f;
+
+            gl.Rotate(rotationY, 0.0f, 1.0f, 0.0f); // Rotate Y
+
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            foreach (Mesh mesh in meshes)
+                if (mesh.ShouldRender)
+                    mesh.Draw(gl);
+
+            gl.Disable(OpenGL.GL_TEXTURE_2D);
+
+            if (autoRotate)
+                rotationY += 3.0f;
+        }
+
+        private void UI_3DView_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseStartX = e.X;
+            mouseStartY = e.Y;
+            isMouseRotating = true;
+        }
+
+        private void UI_3DView_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseRotating = false;
+            prevRotY = rotationY;
+        }
+
+        private void UI_3DView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseRotating)
+            {
+                int diffX = e.X - mouseStartX;
+                int diffY = e.Y - mouseStartY;
+
+                rotationY = prevRotY + (diffX * 0.25f);
+
+                autoRotate = false;
+            }
+        }
+
+        private void UI_3DView_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (autoRotate)
+                autoRotate = false;
+
+            zoom += e.Delta >= 0 ? -0.15f : 0.15f;
+            if (zoom < 0.01f)
+                zoom = 0.01f;
+
+            updateCamera();
+        }
+
+        private void UI_ColourChangeButton_Click(object sender, EventArgs e)
+        {
+            UI_ColourDialog.Color = Color.FromArgb(Program.Settings.ModelViewerBackgroundColour);
+            if (UI_ColourDialog.ShowDialog() == DialogResult.OK)
+            {
+                updateViewerBackground(UI_ColourDialog.Color);
+                Program.Settings.ModelViewerBackgroundColour = UI_ColourDialog.Color.ToArgb();
+                Program.Settings.Persist();
+            }
         }
     }
 }
