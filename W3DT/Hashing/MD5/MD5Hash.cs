5d66b04385cae57f3c446a71b8feb4107f062efc
Kruithne <kruithne@gmail.com>
2016-11-03 18:16:13 +0000

    Move MD5 hasing to hashing namespace.

---
+++ W3DT/Hashing/MD5/MD5Hash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3DT.Hashing.MD5
+{
+    public unsafe struct MD5Hash
+    {
+        public fixed byte Value[16];
+    }
+}
---
+++ W3DT/Hashing/MD5/MD5HashComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3DT.Hashing.MD5
+{
+    class MD5HashComparer
+    {
+        const uint Prime32 = 16777619;
+        const uint Offset32 = 2166136261;
+
+        public unsafe bool Equals(MD5Hash a, MD5Hash b)
+        {
+            for (int i = 0; i < 16; i++)
+                if (a.Value[i] != y.Value[i])
+                    return false;
+
+            return true;
+        }
+
+        public int GetHashCode(MD5Hash hash)
+        {
+            return To32BitFnv1aHash(hash);
+        }
+
+        public unsafe int To32BitFnv1aHash(MD5Hash hash)
+        {
+            uint newHash = Offset32;
+            uint* ptr = (uint*)&hash;
+
+            for (int i = 0; i < 4; i++)
+            {
+                newHash ^= ptr[i];
+                newHash *= Prime32;
+            }
+
+            return unchecked((int)newHash);
+        }
+    }
+}
