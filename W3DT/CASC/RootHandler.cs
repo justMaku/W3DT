365edc848a8e1ace73f3c7cad294257fae65ec65
Kruithne <kruithne@gmail.com>
2016-11-03 06:13:37 +0000

    Added missing content flags

--- W3DT/CASC/RootHandler.cs
+++ W3DT/CASC/RootHandler.cs
@@ -37,7 +37,13 @@ namespace W3DT.CASC
     public enum ContentFlags : uint
     {
         None = 0,
+        F00000001 = 0x1,
+        F00000002 = 0x2,
+        F00000004 = 0x4,
         LowViolence = 0x80, // many models have this flag
+        F10000000 = 0x10000000,
+        F20000000 = 0x20000000, // added in 21737
+        Bundle = 0x40000000,
         NoCompression = 0x80000000 // sounds have this flag
     }
 
