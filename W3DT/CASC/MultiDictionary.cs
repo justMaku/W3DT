00253bed06dc8e84d819a76c7543d595b7e86e70
Kruithne <kruithne@gmail.com>
2016-11-03 18:34:19 +0000

    Change MultiDictionary class to use List rather than HashSet

--- W3DT/CASC/MultiDictionary.cs
+++ W3DT/CASC/MultiDictionary.cs
@@ -2,18 +2,18 @@
 
 namespace W3DT.CASC
 {
-    public class MultiDictionary<K, V> : Dictionary<K, HashSet<V>>
+    public class MultiDictionary<K, V> : Dictionary<K, List<V>>
     {
         public void Add(K key, V value)
         {
-            HashSet<V> hset;
+            List<V> hset;
             if (TryGetValue(key, out hset))
             {
                 hset.Add(value);
             }
             else
             {
-                hset = new HashSet<V>();
+                hset = new List<V>();
                 hset.Add(value);
                 base[key] = hset;
             }
@@ -22,9 +22,7 @@ namespace W3DT.CASC
         public new void Clear()
         {
             foreach (var kv in this)
-            {
                 kv.Value.Clear();
-            }
 
             base.Clear();
         }
